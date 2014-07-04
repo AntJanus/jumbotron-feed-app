f0afaa82e7ed8a2957b2948df561816ca5b99057
Antonin Januska <antonin@antjanus.com>
2014-07-03 21:57:14 -0500

    Vue updates

--- public/src/jumbotron-holder/index.js
+++ public/src/jumbotron-holder/index.js
@@ -3,4 +3,19 @@ require('insert-css')(require('./style.css'))
 module.exports = {
     id: 'jumbotron-holder',
     template: require('./template.html'),
+    data: {
+        expanded: false ,
+        visible: false,
+        fixed: false,
+        bottom: false,
+    },
+
+    methods: {
+        toggle: function(property, e) {
+            console.log('happened');
+            this.property = !property;
+            e.stopPropagation();
+            e.preventDefault();
+        }
+    },
 }
